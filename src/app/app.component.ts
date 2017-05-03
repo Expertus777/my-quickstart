f32d70d8dbad340950f60f392437d554e268ccea
Ruslan_Behun_ValueAdd <ruslanbegun@gmail.com>
2017-05-03 13:20:36 +0300

    done service lessons 5/7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
-
-const HEROES: Hero[] = [
-  {id: 11, name: 'Mr. Nice'},
-  {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bombasto'},
-  {id: 14, name: 'Celeritas'},
-  {id: 15, name: 'Magneta'},
-  {id: 16, name: 'RubberMan'},
-  {id: 17, name: 'Dynama'},
-  {id: 18, name: 'Dr IQ'},
-  {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
-];
-
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -107,13 +94,24 @@ const HEROES: Hero[] = [
       font-size: 14px;
       cursor: pointer;
     }
-  `]
+  `],
+  providers: [HeroService]
 })
 
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroesSlowly(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroesSlowly();
+  }
 
   onSelect(hero: Hero): void {
     if (this.selectedHero === hero) {
